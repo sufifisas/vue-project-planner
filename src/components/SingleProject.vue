5f0f1d550f9b074e3861c3af2051244c584681ba
sufi <sufifisas@gmail.com>
2022-01-25 01:13:11 +0800

    feat: create single project component

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="project__item">
+    <div class="project__content">
+        <p class="project__title">Project 1</p>
+        <div class="project__icons">
+            <span>Edit</span>
+            <span>Delete</span>
+            <span>Complete</span>
+        </div>
+    </div>
+    <div class="project__desc">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas est cum consequuntur hic amet fugiat, suscipit veniam sunt soluta unde.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
