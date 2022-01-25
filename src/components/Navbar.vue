f14ee76918d90ad1b694044534823c35aa202f2a
sufi <sufifisas@gmail.com>
2022-01-25 23:16:39 +0800

    feat: add main navbar component to the app

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="nav__header">
+      <router-link :to="{ name: 'Home'}"><span>Home</span></router-link>
+      <router-link :to="{ name: 'CreateProject' }"><span>Create New Project</span></router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.nav__header {
+    padding: 24px 0;
+    font-size: 20px;
+}
+.nav__header a {
+    text-decoration: none;
+    padding-bottom: 5px;
+    display: inline-block;
+    color: #888888;
+}
+.nav__header .router-link-active {
+    border-bottom: 2px solid green;
+    color: #000000;
+}
+.nav__header a:not(:last-child) {
+    margin-right: 16px;
+}
+</style>
